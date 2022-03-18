812cc33852c1732bb9ee06a26ab385cfd1fc033c
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-18 14:08:38 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InventoryDataManagementDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Object Oriented Programing");
+
+            //string path = "C:/$Donald/BridgeLabz/Visual Studio/InventoryDataManagementDemo/Rice.json";
+
+            //FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+            //Rice data = fetchJsonForRice.Read(path);
+
+            //Console.WriteLine("Types of Rice: ");
+            //for(int i = 0; i < data.typesofRice.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofRice[i].name);
+            //    Console.WriteLine(data.typesofRice[i].price);
+            //    Console.WriteLine(data.typesofRice[i].weight);
+            //    //int dataofPulses = data.typesofRice[i].price * data.typesofRice[i].weight;
+            //    //Console.WriteLine("The Price of "+ data.typesofRice[i].name+" for "+ data.typesofRice[i].weight+" kg is "+dataofPulses);
+            //    Console.WriteLine("--------------------------");
+            //}
+
+            //Console.WriteLine("\nTypes of Pulses: ");
+            //for (int i = 0; i < data.typesofPulses.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofPulses[i].name);
+            //    Console.WriteLine(data.typesofPulses[i].price);
+            //    Console.WriteLine(data.typesofPulses[i].weight);
+            //    Console.WriteLine("--------------------------");
+            //}
+
+            //Console.WriteLine("\nTypes of Wheat: ");
+            //for (int i = 0; i < data.typesofWheat.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofWheat[i].name);
+            //    Console.WriteLine(data.typesofWheat[i].price);
+            //    Console.WriteLine(data.typesofWheat[i].weight);
+            //    Console.WriteLine("--------------------------");
+            //}
+        }
+    }
+}
